1d22f0cca612f6b2a8e9eb618ea709d39b75065c
Niksor <Salfordyoutube@ya.ru>
2024-01-13 22:24:17 +0300

    Начальный экран и что почём, мобильной всерсии

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //И ещё один для удаления старого main 
+
+// подключите к проекту mini-css-extract-plugin для сборки всех CSS в один  
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+
+module.exports = {
+    entry: { main: './src/script.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+                publicPath: ''
+    },
+    
+    mode: 'development', // добавили режим разработчика
+    devServer: {
+        static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+        compress: true, // это ускорит загрузку в режиме разработки
+        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    
+        open: true // сайт будет открываться сам при запуске npm run dev
+      },
+
+    module: {
+      // правила для обработки js, html и других файлов
+        rules: [ // rules — это массив правил
+          // добавим в него объект правил для бабеля
+          {
+            // регулярное выражение, которое ищет все js файлы
+            test: /\.js$/,
+            // при обработке этих файлов нужно использовать babel-loader
+            use: 'babel-loader',
+            // исключает папку node_modules, файлы в ней обрабатывать не нужно
+            exclude: '/node_modules/'
+          },
+          {
+            // регулярное выражение, которое ищет все файлы с такими расширениями
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource'
+          },
+          {
+            // применять это правило только к CSS-файлам
+            test: /\.css$/,
+            // при обработке этих файлов нужно использовать
+            // MiniCssExtractPlugin.loader и css-loader
+            use: [MiniCssExtractPlugin.loader, {
+              loader: 'css-loader',
+              options: { importLoaders: 1 }
+            },
+            'postcss-loader']
+          },
+        ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html' // путь к файлу index.html
+      }),
+      new CleanWebpackPlugin(), //Удаялем всё старье
+      new MiniCssExtractPlugin(), //Собтраем все css в один
+    ]
+      
+}
