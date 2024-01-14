15d1880ea6e61b348faef41d6b8565bf6c80b63e
Niksor <Salfordyoutube@ya.ru>
2024-01-14 20:55:06 +0300

    Bug fix. Функционал показа ответов на вопросы по нажатию

--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
 import './pages/index.css';
 import Swiper from "swiper"; //А стили импортируются index.css
 
-
-
 new Swiper('.swiper-container', {
     loop: true,
     //spaceBetween: 0,
@@ -11,3 +9,19 @@ new Swiper('.swiper-container', {
       },
     //initialSlide: 1, //Слайд с которго начать
 });
+
+const questions = document.querySelectorAll(".question__button")
+questions.forEach((qstn) => {
+    qstn.addEventListener('click', () =>{
+        const text = qstn.nextElementSibling;
+        if (text.classList.contains('close')){
+            // qstn.textContent = "⌄"
+            qstn.style.transform = "rotate(90deg)";
+            text.classList.remove('close');
+        }
+        else{
+            qstn.style.transform = "rotate(0)";
+            text.classList.add('close');
+        }
+    })
+})
