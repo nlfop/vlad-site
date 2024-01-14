2ca462c24f026ecd7cca80de5a9a81de57accdc0
Niksor <Salfordyoutube@ya.ru>
2024-01-14 20:21:55 +0300

    Сделан слайдер. Добавлены новые фотки Влада

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
 import './pages/index.css';
+import Swiper from "swiper"; //А стили импортируются index.css
+
+
+
+new Swiper('.swiper-container', {
+    loop: true,
+    //spaceBetween: 0,
+    autoplay: {
+        delay: 4000,
+      },
+    //initialSlide: 1, //Слайд с которго начать
+});
