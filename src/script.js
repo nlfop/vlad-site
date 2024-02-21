e77fce904db54222a2413ef53e0d4887f994290e
nlfop <anlvovskay@mail.ru>
2024-02-21 16:02:59 +0300

    fix

--- src/script.js
+++ src/script.js
@@ -26,19 +26,19 @@ questions.forEach((qstn) => {
     })
 })
 
-const questions_main = document.querySelectorAll(".question__title")
-questions_main.forEach((qstn_m) => {
-    qstn_m.addEventListener('click', () =>{
-        const quote_question = qstn_m.nextElementSibling;
-        const text = qstn_m.nextElementSibling.nextElementSibling;
-        if (text.classList.contains('close')){
-            // qstn.textContent = "⌄"
-            quote_question.style.transform = "rotate(90deg)";
-            text.classList.remove('close');
-        }
-        else{
-            quote_question.style.transform = "rotate(0)";
-            text.classList.add('close');
-        }
-    })
-})
+// const questions_main = document.querySelectorAll(".question__title")
+// questions_main.forEach((qstn_m) => {
+//     qstn_m.addEventListener('click', () =>{
+//         const quote_question = qstn_m.nextElementSibling;
+//         const text = quote_question.nextElementSibling;
+//         if (text.classList.contains('close')){
+//             // qstn.textContent = "⌄"
+//             quote_question.style.transform = "rotate(90deg)";
+//             text.classList.remove('close');
+//         }
+//         else{
+//             quote_question.style.transform = "rotate(0)";
+//             text.classList.add('close');
+//         }
+//     })
+// })
