10fa45762cbcb5695d580d1d27b94530f2c7da36
nlfop <anlvovskay@mail.ru>
2024-02-29 13:16:20 +0300

    question

--- src/script.js
+++ src/script.js
@@ -27,19 +27,19 @@ questions.forEach((qstn) => {
 )
   })
 
-// const questions_main = document.querySelectorAll(".question__title")
-// questions_main.forEach((qstn_m) => {
-//     qstn_m.addEventListener('click', () =>{
-//         const quote_question = qstn_m.nextElementSibling;
-//         const text = quote_question.nextElementSibling;
-//         if (text.classList.contains('close')){
-//             // qstn.textContent = "⌄"
-//             quote_question.style.transform = "rotate(90deg)";
-//             text.classList.remove('close');
-//         }
-//         else{
-//             quote_question.style.transform = "rotate(0)";
-//             text.classList.add('close');
-//         }
-//     })
-// })
+const questions_main = document.querySelectorAll(".question_but")
+questions_main.forEach((qstn_m) => {
+    qstn_m.addEventListener('click', () =>{
+        const quote_question = qstn_m.nextElementSibling;
+        const text = quote_question.nextElementSibling;
+        if (text.classList.contains('close')){
+            // qstn.textContent = "⌄"
+            quote_question.style.transform = "rotate(90deg)";
+            text.classList.remove('close');
+        }
+        else{
+            quote_question.style.transform = "rotate(0)";
+            text.classList.add('close');
+        }
+    })
+})
