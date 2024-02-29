19d6ce0127e86adfb86d3236484ccb31d6564730
nlfop <anlvovskay@mail.ru>
2024-02-29 14:16:17 +0300

    add move

--- src/script.js
+++ src/script.js
@@ -10,40 +10,41 @@ new Swiper('.swiper-container', {
     //initialSlide: 1, //Слайд с которго начать
 });
 
-const questions = document.querySelectorAll(".question__button")
-questions.forEach((qstn) => {
-    qstn.addEventListener('click', () =>{
-        const text = qstn.nextElementSibling;
-        if (text.classList.contains('close')){
-            // qstn.textContent = "⌄"
-            qstn.style.transform = "rotate(90deg)";
-            text.classList.remove('close');
-        }
-        else{
-            qstn.style.transform = "rotate(0)";
-            text.classList.add('close');
-        }
-    }
-)
-  })
+// const questions = document.querySelectorAll(".question__button")
+// questions.forEach((qstn) => {
+//     qstn.addEventListener('click', () =>{
+//         const text = qstn.nextElementSibling;
+//         if (text.classList.contains('close')){
+//             // qstn.textContent = "⌄"
+//             qstn.style.transform = "rotate(90deg)";
+//             text.classList.remove('close');
+//         }
+//         else{
+//             qstn.style.transform = "rotate(0)";
+//             text.classList.add('close');
+//         }
+//     }
+// )
+//   })
 
 
 
   const questions_main = document.querySelectorAll(".question")
 questions_main.forEach((qstn_m) => {
     qstn_m.addEventListener('click', () =>{
-        // const quote_question = qstn_m.getElementsByClassName("question__button");
-        alert(1)
-        // const text = quote_question.nextElementSibling;
-        // if (text.classList.contains('close')){
-        //     // qstn.textContent = "⌄"
-        //     quote_question.style.transform = "rotate(90deg)";
-        //     text.classList.remove('close');
-        // }
-        // else{
-        //     quote_question.style.transform = "rotate(0)";
-        //     text.classList.add('close');
-        // }
+      alert(2)
+        const quote_question = qstn_m.getElementsByClassName("question__button");
+        if(quote_question==null) alert(1)
+        const text = quote_question.nextElementSibling;
+        if (text.classList.contains('close')){
+            // qstn.textContent = "⌄"
+            quote_question.style.transform = "rotate(90deg)";
+            text.classList.remove('close');
+        }
+        else{
+            quote_question.style.transform = "rotate(0)";
+            text.classList.add('close');
+        }
     })
 })
 
